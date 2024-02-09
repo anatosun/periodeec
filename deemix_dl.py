3de3d493e89762859bc10b8354ebbf4f126ec120
François-Xavier Wicht <francois-xavier.wicht@unibe.ch>
2024-02-09 15:58:55 +0100

    fix: disabled folder moving on errors

--- deemix_dl.py
+++ deemix_dl.py
@@ -162,17 +162,8 @@ class Deemix:
             return False, path, stdout.replace("\n", " ")
 
         album_path = os.path.join(path, str(id))
-
-        if os.path.exists(os.path.join(album_path, "errors.txt")):
-            failed = os.path.join(path, "failed")
-            if not os.path.exists(failed):
-                os.makedirs(failed)
-            failed_album = os.path.join(failed, str(id))
-            failed_album_errors = os.path.join(failed_album, "errors.txt")
-
-            if not os.path.exists(failed_album):
-                os.rename(album_path, failed_album)
-
-            return False, failed_album, f"check {failed_album_errors} for more details"
+        errors = os.path.join(album_path, "errors.txt")
+        if os.path.exists(errors):
+            return False, album_path, f"check {errors} for more details"
 
         return True, album_path, ""
