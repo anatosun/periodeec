a097c1b3fb17a38ef777b40cc68079b00775a047
anatosun <z4jyol8l@duck.com>
2025-03-15 13:42:43 +0100

    fix: passed proper username

--- periodeec/main.py
+++ periodeec/main.py
@@ -33,7 +33,7 @@ logging.basicConfig(
     level=logging.INFO)
 
 
-def sync_playlist_to_plex(playlist: Playlist, username: str, plex_handler: PlexHandler, spotify_handler: SpotifyHandler, bt, downloaders, download_path, collection=False):
+def sync_playlist_to_plex(playlist: Playlist, plex_users: list, plex_handler: PlexHandler, spotify_handler: SpotifyHandler, bt, downloaders, download_path, collection=False):
     """Handles the process of fetching, matching, and syncing a Spotify playlist to Plex."""
     logging.info(f"Syncing playlist '{playlist.title}' to Plex")
     spotify_handler.populate_playlist(playlist)
@@ -67,20 +67,22 @@ def sync_playlist_to_plex(playlist: Playlist, username: str, plex_handler: PlexH
                                     track.path = path
                         else:
                             logging.error(err)
-
-    plex_handler.create(playlist, username, collection)
+    for username in plex_users:
+        plex_handler.create(playlist, username, collection)
 
 
 def sync(spotify_handler, plex_handler, config, bt, downloaders, settings):
     """Syncs Spotify playlists and users to Plex using the correct logic."""
     if config.usernames:
-        for user in config.usernames.values():
+        for username in config.usernames.keys():
+            user = config.usernames[username]
+            plex_users = user.sync_to_plex_users
             playlists = spotify_handler.fetch_playlists_from_user(
                 user.spotify_username)
             for playlist in playlists:
                 schedule.every(user.schedule).minutes.do(
                     lambda pl=playlist: sync_playlist_to_plex(
-                        pl, user.spotify_username, plex_handler, spotify_handler, bt, downloaders, settings.downloads
+                        pl, plex_users, plex_handler, spotify_handler, bt, downloaders, settings.downloads
                     )
                 )
 
