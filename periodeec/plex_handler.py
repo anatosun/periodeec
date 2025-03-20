5682f24af0e9d85c267059cbf47e3dfa275156e8
anatosun <z4jyol8l@duck.com>
2025-03-20 12:32:23 +0100

    style: improved PlexHandler

--- periodeec/plex_handler.py
+++ periodeec/plex_handler.py
@@ -1,5 +1,4 @@
 import os
-import re
 import logging
 from plexapi.server import PlexServer
 from plexapi.collection import Collection as PlexCollection
@@ -18,26 +17,22 @@ class PlexHandler:
         self.admin_user = self.plex_server.account().username
 
     def get_plex_instance_for_user(self, username: str):
-        """Return a temporary Plex instance for a given user without modifying the class attribute."""
-        if username and username != self.admin_user:
-            return self.plex_server.switchUser(username)
-        return self.plex_server
+        """Return a Plex instance for a given user."""
+        return self.plex_server.switchUser(username) if username and username != self.admin_user else self.plex_server
+
+    def sanitize_filename(self, name: str) -> str:
+        """Sanitize filenames by replacing invalid characters."""
+        return ''.join('_' if char in '<>:"/\\|?* ' else char for char in name)
 
     def create_m3u(self, playlist: Playlist, username: str) -> str:
         """Creates an M3U file for the given playlist and returns the file path."""
+        username = self.sanitize_filename(username)
+        title = self.sanitize_filename(playlist.title)
 
-        invalid = '<>:"/\\|?* '
-        title = playlist.title
+        user_m3u_path = os.path.join(self.m3u_path, username)
+        os.makedirs(user_m3u_path, exist_ok=True)
 
-        for char in invalid:
-            title = title.replace(char, '_')
-            username = username.replace(char, '_')
-
-        m3u_path = os.path.join(self.m3u_path, username)
-        if not os.path.exists(m3u_path):
-            os.makedirs(m3u_path)
-
-        m3u_file_path = os.path.join(m3u_path,  f"{title}.m3u")
+        m3u_file_path = os.path.join(user_m3u_path, f"{title}.m3u")
         with open(m3u_file_path, "w") as m3u_file:
             m3u_file.write("#EXTM3U\n")
             for track in playlist.tracks:
@@ -49,73 +44,74 @@ class PlexHandler:
         logger.info(f"Created M3U file: {m3u_file_path}")
         return m3u_file_path
 
-    def create(self, playlist: Playlist, username, collection: bool = False) -> bool:
+    def create_collection(self, playlist: Playlist, items) -> bool:
+        """Create or update a Plex collection."""
+        section = self.plex_server.library.section(self.section)
+        try:
+            col = section.collection(title=playlist.title)
+            if col is not None:
+                logger.info(
+                    f"Updating existing Plex collection '{playlist.title}'")
+                col.removeItems(col.items())
+                col.addItems(items)
+                col.uploadPoster(url=playlist.poster)
+                col.editSummary(summary=playlist.summary)
+                return True
+        except Exception as e:
+            logger.info(
+                f"Creating new Plex collection '{playlist.title}': {e}")
+
+        try:
+            col = self.plex_server.createCollection(
+                title=playlist.title, section=self.section, items=items)
+            col.uploadPoster(url=playlist.poster)
+            col.editSummary(summary=playlist.summary)
+            return True
+        except Exception as e:
+            logger.error(f"Failed to create collection {playlist.title}: {e}")
+            return False
+
+    def create_playlist(self, playlist: Playlist, username: str, items) -> bool:
+        """Create or update a Plex playlist."""
+        plex_instance = self.get_plex_instance_for_user(username)
+
+        try:
+            existing_playlist = plex_instance.playlist(title=playlist.title)
+            if existing_playlist is not None:
+                logger.info(
+                    f"Updating existing Plex playlist '{playlist.title}' for '{username}'")
+                existing_playlist.removeItems(existing_playlist.items())
+                existing_playlist.addItems(items)
+                existing_playlist.uploadPoster(url=playlist.poster)
+                existing_playlist.editSummary(summary=playlist.summary)
+            return True
+        except:
+            logger.info(
+                f"Creating new Plex playlist '{playlist.title}' for '{username}'")
+            try:
+                new_playlist = plex_instance.createPlaylist(
+                    playlist.title, items=items, smart=False)
+                new_playlist.uploadPoster(url=playlist.poster)
+                new_playlist.editSummary(summary=playlist.summary)
+                return True
+            except Exception as e:
+                logger.error(
+                    f"Error creating playlist {playlist.title} for '{username}': {e}")
+                return False
+
+    def create(self, playlist: Playlist, username: str, collection: bool = False) -> bool:
         """Create or update a Plex playlist or collection."""
         m3u_file = self.create_m3u(playlist, username)
-        success = False
 
         try:
             temp_playlist = self.plex_server.createPlaylist(
-                title=f"{playlist.title} (temp)", section=self.section, m3ufilepath=m3u_file)
+                title=f"{playlist.title} (temp)", section=self.section, m3ufilepath=m3u_file
+            )
             items = temp_playlist.items()
+            temp_playlist.delete()
         except Exception as e:
             logger.error(
                 f"Failed to create temporary playlist '{playlist.title}': {e}")
             return False
 
-        if collection:
-            col: PlexCollection
-            try:
-                col = self.plex_server.library.section(
-                    self.section).collection(title=playlist.title)
-                col.delete()
-                logger.info(
-                    f"Updating existing Plex collection '{playlist.title}'")
-            except:
-                logger.info(
-                    f"Creating new Plex collection '{playlist.title}'")
-
-            try:
-                col = self.plex_server.createCollection(
-                    title=playlist.title, section=self.section, items=items)
-                col.uploadPoster(url=playlist.poster)
-                col.editSummary(summary=playlist.summary)
-                success = True
-            except Exception as e:
-                logger.error(
-                    f"Failed to create collection {playlist.title}: {e}")
-                success = False
-
-        else:
-            try:
-                plex_instance = self.get_plex_instance_for_user(username)
-                pl: PlexPlaylist
-                try:
-                    res = plex_instance.playlist(title=playlist.title)
-
-                    if res is not None:
-                        pl = res
-                        logger.info(
-                            f"Updating existing Plex playlist '{playlist.title}' for '{username}'")
-                        pl.removeItems(pl.items())
-                        pl.addItems(items=items)
-                        pl.uploadPoster(url=playlist.poster)
-                        pl.editSummary(summary=playlist.summary)
-
-                    else:
-                        logger.error(
-                            f"Failed to fetch Plex playlist '{playlist.title}' for '{username}', result is '{res}'")
-                except Exception as e:
-                    logger.info(
-                        f"Creating new Plex playlist '{playlist.title}' for '{username}': {e}")
-                    try:
-                        pl = plex_instance.createPlaylist(
-                            playlist.title, items=items, smart=False)
-                        pl.uploadPoster(url=playlist.poster)
-                        pl.editSummary(summary=playlist.summary)
-                    except Exception as e:
-                        logger.error(
-                            f"Error creating playlist {playlist.title} {e} for '{username}'")
-            except Exception as e:
-                logger.error(
-                    f"Failed to switch to Plex user '{username}': {e}")
+        return self.create_collection(playlist, items) if collection else self.create_playlist(playlist, username, items)
